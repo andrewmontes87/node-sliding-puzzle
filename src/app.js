ffb6c691db458996810989ee602db942e67f877e
andrewmontes87 <andrewmontes87@gmail.com>
2017-01-02 10:40:35 -0800

    still fiddling with cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import url from 'url';
 
 export const app = express();
  
-app.get('/api/v1/puzzle', cors(),(request, response) => {
+app.get('/api/v1/puzzle', cors(), (request, response) => {
   const puzzleParam = url.parse(request.url, true).query.p; 
   if (!puzzleParam) {
     response.status(200).send('Send a puzzle pls!');
