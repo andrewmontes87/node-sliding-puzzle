52d95248d9328fe848b5efda773184dd6b96aa33
andrewmontes87 <andrewmontes87@gmail.com>
2017-01-02 10:32:07 -0800

    added cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { getPuzzleJSON } from './AStar'; 
 import url from 'url';
 
 export const app = express();
  
-app.get('/api/v1/puzzle/', (request, response) => {
+app.get('/api/v1/puzzle/', cors(),(request, response) => {
   const puzzleParam = url.parse(request.url, true).query.p; 
   if (!puzzleParam) {
     response.status(200).send('Send a puzzle pls!');
