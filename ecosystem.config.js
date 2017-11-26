7e6b6c55b84fe406c1e175c2d99b8ab068de2ae5
andrewmontes87 <andrewmontes87@gmail.com>
2017-11-26 14:48:33 -0800

    migrating to ec2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'node-sliding-puzzle',
+    script: 'node index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ec2-user',
+      host: 'ec2-54-209-121-121.compute-1.amazonaws.com',
+      key: '~/.ssh/node-ec2-test.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/andrewmontes87/node-sliding-puzzle.git',
+      path: '/home/ec2-user/node-sliding-puzzle',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
